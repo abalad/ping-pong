7cc8e1767c021e5ce297d47c728bbca8d3fa7e4a
Maicon Wagner dos Santos <mister-x59@hotmail.com.br>
2023-06-13 21:37:29 -0300

    feat(elements): add actions and scores to players

--- scripts/elements/ball-element.js
+++ scripts/elements/ball-element.js
@@ -5,13 +5,44 @@ const ball = {
     speed: 5,
     directionY: 1,
     directionX: 1,
-    _calPosition: function (){
-        if(
-            ( this.y - this.r < 0 && this.directionY < 0 ) ||
-            ( this.y > field.height - this.r && this.directionY > 0)
-        ) {
+    _validateEdgesTouched: function () {
+        const isTouchedOnTop = this.y - this.r < 0 && this.directionY < 0;
+        const isTouchedOnBottom = this.y > field.height - this.r && this.directionY > 0;
+
+        if(isTouchedOnTop || isTouchedOnBottom) {
             this._reverseY(); // Revert position from ball
         }
+    },
+    _checkPlayerPoint: function () {
+        const isTouchedOnRightEdge = this.x > field.width - this.r - racketRight.width - GLOBAL_RACKET_GAPX;
+        const isTouchedOnRacket = this.y + this.r > racketRight.y && this.y - this.r < racketRight.y + racketRight.height
+
+        if(isTouchedOnRightEdge) {
+            if(isTouchedOnRacket){
+                this._reverseX()
+            } else {
+                scoreboard.increaseHuman();
+                this._resetPosition();
+            }
+        }
+    },
+    _checkComputerPoint: function (){
+        const isTouchedOnLeftEdge = this.x < this.r + racketLeft.width + GLOBAL_RACKET_GAPX;
+        const isTouchedOnRacket = this.y + this.r > racketLeft.y && this.y - this.r < racketLeft.y + racketLeft.height;
+
+        if(isTouchedOnLeftEdge){
+            if(isTouchedOnRacket) {
+                this._reverseX()
+            } else {
+                scoreboard.increaseComputer();
+                this._resetPosition();
+            }
+        }
+    },
+    _calPosition: function (){
+        this._validateEdgesTouched();
+        this._checkPlayerPoint();
+        this._checkComputerPoint();
     },
     _reverseY: function () {
         this.directionY *= -1;
@@ -22,6 +53,10 @@ const ball = {
     _move: function () {
         this.x += this.directionX * this.speed;
         this.y += this.directionY * this.speed;
+    },
+    _resetPosition: function () {
+        this.x = field.width / 2;
+        this.y = field.height / 2;
     },
     draw: function  () {
         canvasContext.beginPath();
--- scripts/elements/scoreboard-element.js
+++ scripts/elements/scoreboard-element.js
@@ -2,15 +2,21 @@ const scoreboard = {
     positionY: 30,
     positionLeftX: GLOBAL_WIDTH / 4,
     positionRightX: GLOBAL_WIDTH / 4 +GLOBAL_WIDTH / 2,
-    leftText: '4',
-    rightText: '3',
+    human: 0,
+    computer: 0,
+    increaseHuman: function () {
+        this.human ++;
+    },
+    increaseComputer: function () {
+        this.computer ++;
+    },
     draw: function () {
         canvasContext.font = 'bold 72px Arial';
         canvasContext.textAlign = 'center';
         canvasContext.textBaseline = 'top';
         canvasContext.fillStyle = GLOBAL_SCOREBOARD_COLOR;
-        canvasContext.fillText(this.leftText, this.positionLeftX, this.positionY);
-        canvasContext.fillText(this.rightText, this.positionRightX, this.positionY);
+        canvasContext.fillText(this.human, this.positionLeftX, this.positionY);
+        canvasContext.fillText(this.computer, this.positionRightX, this.positionY);
     }
 
 }
